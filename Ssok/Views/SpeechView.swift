2304293ceac6d1e0923fa4a558de54e6d409ab4a
snack <kwansiknam@gmail.com>
2023-05-14 21:38:30 +0900

    feature: add progressview

--- Ssok/Views/SpeechView.swift
+++ Ssok/Views/SpeechView.swift
@@ -10,16 +10,18 @@ import SwiftUI
 struct SpeechView: View {
     
     @ObservedObject var speechRecognizer = SpeechRecognizer()
-    @State var userSay : String = ""
     @State var answerText = "나는 바보다"
     @State var speechTime: Double = 0.0
     @State var isSpeech: Bool = false
+    @State var isWrong: Bool = false
+    
+    let progressTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var answer = "This is water"
     var timer : Double = 5.0
     
     var body: some View {
-        VStack(spacing: 40) {
+        VStack(spacing: 24) {
             ZStack(alignment: .top) {
                 Image("speech_top")
                     .edgesIgnoringSafeArea(.all)
@@ -87,35 +89,35 @@ struct SpeechView: View {
                             .foregroundColor(Color("Orange"))
                         Text(answerText)
                             .font(.system(size: 48, weight: .heavy))
-                            .frame(width: 240)
+                            .frame(width: 240, height: 64)
                             .minimumScaleFactor(0.1)
                             .multilineTextAlignment(.center)
-                            .lineLimit(1)
+                            .lineLimit(2)
                     }
                 }
                 if !isSpeech {
                     ZStack {
                         Image("SpeechButton")
                             .shadow(radius: 4, y: 4)
-                        VStack(spacing: 25) {
-                            Text("말하기")
+                        VStack(spacing: 24) {
+                            Text("눌러서 말하기")
                                 .underline()
                                 .foregroundColor(.white)
                                 .font(.system(size: 32, weight: .heavy))
-                            Text("터치 후 위의 문장을 정확하게 읽어요")
+                            Text(isWrong ? "제시어와 달라요 다시 읽어 주세요" : "터치 후 위의 문장을 정확하게 읽어요")
                                 .foregroundColor(.white)
                                 .font(.system(size: 14, weight: .semibold))
                         }
                         .padding(.top)
                     }
                     .onTapGesture {
-                        speechTime = 1.0
-                        isSpeech.toggle()
+                        speechTime = 100
+                        isSpeech = true
                     }
                 } else {
                     ZStack {
                         Image("Speeching")
-                            .shadow(radius: 4, y: 4)
+                            .shadow(color: Color("Orange").opacity(0.5), radius: 5)
                         VStack(spacing: 16) {
                             Text("내 발음")
                                 .font(.system(size: 13, weight: .semibold))
@@ -124,71 +126,81 @@ struct SpeechView: View {
                                 .background(Color("Orange"))
                                 .cornerRadius(15)
                                 .foregroundColor(.white)
-                            Text("제시어를 읽어주세요")
-                                .opacity(0.25)
-                                .font(.system(size: 48, weight: .heavy))
-                                .frame(width: 240)
-                                .minimumScaleFactor(0.1)
-                                .multilineTextAlignment(.center)
-                                .lineLimit(1)
-                            Text("❌ 제시어와 달라요 다시 읽어 주세요 ❌")
-                                .font(.system(size: 13, weight: .semibold))
-                                .padding(.vertical, 2)
-                                .background(Color("LightRed"))
-                                .foregroundColor(Color("Red"))
+                            if speechRecognizer.transcript == "" {
+                                Text("제시어를 읽어주세요")
+                                    .opacity(0.25)
+                                    .font(.system(size: 48, weight: .heavy))
+                                    .frame(width: 240, height: 64)
+                                    .minimumScaleFactor(0.1)
+                                    .lineLimit(1)
+                            } else {
+                                Text(speechRecognizer.transcript)
+                                    .font(.system(size: 48, weight: .heavy))
+                                    .frame(width: 240, height: 64)
+                                    .minimumScaleFactor(0.1)
+                                    .multilineTextAlignment(.center)
+                                    .lineLimit(2)
+                            }
+                            if isWrong {
+                                Text("❌ 제시어와 달라요 다시 읽어 주세요 ❌")
+                                    .font(.system(size: 13, weight: .semibold))
+                                    .padding(.vertical, 2)
+                                    .background(Color("LightRed"))
+                                    .foregroundColor(Color("Red"))
+                            }
                         }
                         .padding(.top)
                     }
                     .padding(.top, 3)
-                    .onTapGesture {
-                        isSpeech.toggle()
-                        speechTime = 1.0
+                    .onAppear {
+                        speechRecognizer.stopTranscript() //혹시라도 켜있으면 껏다다시키게
+                        speechRecognizer.startTranscribing()
+                        let timer = Timer.scheduledTimer(withTimeInterval: timer, repeats: false){
+                            timer in
+                            let cleanedTranscript = speechRecognizer.transcript.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "")
+                            //영소문자 바꾸는 거 해야함.
+                            
+                            if(answer.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "") != cleanedTranscript){
+                                speechRecognizer.stopTranscript() //혹시라도 켜있으면 껏다다시키게
+                                speechRecognizer.startTranscribing()
+                                isWrong = true
+                                isSpeech = false
+                                speechRecognizer.transcript = ""
+                            }
+                        }
+                        let checktimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false){
+                            timer in
+                            let cleanedTranscript = speechRecognizer.transcript.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "")
+                            //영소문자 바꾸는 거 해야함.
+
+                            if(answer.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "") == cleanedTranscript){
+                                timer.invalidate()
+                                print("끝이 났어요")
+                            }}
+                        RunLoop.main.add(checktimer, forMode: .common)
+                        RunLoop.main.add(timer, forMode: .common)
+                    }
+                    .onDisappear{
+                        speechRecognizer.stopTranscript()
                     }
                 }
             }
-            ZStack {
-                Image("progress")
-                    .shadow(color: Color(.black).opacity(0.25),radius: 4)
-                    .overlay(
-                        ProgressView(value: speechTime)
-                            .tint(Color("Orange_Progress"))
-                            .padding(.horizontal, 40)
-                            .padding(.top, 8)
-                            .scaleEffect(y: 2)                    )
-            }
-        }
-        
-        //        // 말하는게 입력되는 텍스트
-        //        Text(speechRecognizer.transcript).foregroundColor(.red).font(.system(size: 40))
-        
-        .onAppear{
-            speechRecognizer.stopTranscript() //혹시라도 켜있으면 껏다다시키게
-            speechRecognizer.startTranscribing()
-            let timer = Timer.scheduledTimer(withTimeInterval: timer, repeats: true){
-                timer in
-                let cleanedTranscript = speechRecognizer.transcript.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "")
-                //영소문자 바꾸는 거 해야함.
-              
-                if(answer.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "") != cleanedTranscript){
-                    speechRecognizer.stopTranscript() //혹시라도 켜있으면 껏다다시키게
-                    speechRecognizer.startTranscribing()
-                    print("틀렷으니 다시해라")
-                }
-            }
-            let checktimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){
-                timer in
-                let cleanedTranscript = speechRecognizer.transcript.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "")
-                //영소문자 바꾸는 거 해야함.
-                
-                if(answer.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "") == cleanedTranscript){
-                    timer.invalidate()
-                    print("끝이 났어요")
-                }}
-            RunLoop.main.add(checktimer, forMode: .common)
-            RunLoop.main.add(timer, forMode: .common)
-        }
-        .onDisappear{
-            speechRecognizer.stopTranscript()
+            Image("progress")
+                .shadow(color: Color(.black).opacity(0.25),radius: 4)
+                .overlay(
+                    ProgressView(value: speechTime, total: 100)
+                        .tint(Color("Orange_Progress"))
+                        .padding(.horizontal, 40)
+                        .padding(.top, 8)
+                        .scaleEffect(y: 2)
+                        .onReceive(progressTimer) { _ in
+                            withAnimation(.linear(duration: 1)) {
+                                if speechTime > 0 {
+                                    speechTime -= 100/timer
+                                }
+                            }
+                        }
+                )
         }
     }
 }
--- Ssok/_BubblesApp.swift
+++ Ssok/_BubblesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct _BubblesApp: App {
     var body: some Scene {
         WindowGroup {
-            IntroView()
+            SpeechView()
                 .environmentObject(RandomMember())
         }
     }
