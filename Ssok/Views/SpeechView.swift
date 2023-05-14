972f51225e9214f0aa93ec7e43e6c20bfefb0e55
snack <kwansiknam@gmail.com>
2023-05-14 23:15:06 +0900

    fix: fix speechview

--- Ssok/Views/SpeechView.swift
+++ Ssok/Views/SpeechView.swift
@@ -57,23 +57,25 @@ struct SpeechView: View {
                     RoundedRectangle(cornerRadius: 15)
                         .stroke(Color("LightBlue_stroke"), lineWidth: 1.5)
                 )
+            // 카드 둘
             VStack(spacing: 30) {
+                // 제시어 카드
                 ZStack {
                     RoundedRectangle(cornerRadius: 20)
                         .frame(width: 307, height: 175)
                         .foregroundColor(.white)
-                        .shadow(radius: 10)
+                        .shadow(color: Color(.black).opacity(0.2),radius: 8)
                         .background(
                             RoundedRectangle(cornerRadius: 20)
                                 .frame(width: 284, height: 175)
                                 .foregroundColor(.white)
-                                .shadow(radius: 10)
+                                .shadow(color: Color(.black).opacity(0.2),radius: 8)
                                 .offset(y: 12)
                                 .background(
                                     RoundedRectangle(cornerRadius: 20)
                                         .frame(width: 268, height: 175)
                                         .foregroundColor(.white)
-                                        .shadow(radius: 10)
+                                        .shadow(color: Color(.black).opacity(0.2),radius: 8)
                                         .offset(y: 22)
                                 )
                         )
@@ -95,63 +97,63 @@ struct SpeechView: View {
                             .lineLimit(2)
                     }
                 }
-                if !isSpeech {
-                    ZStack {
-                        Image("SpeechButton")
-                            .shadow(radius: 4, y: 4)
-                        VStack(spacing: 24) {
-                            Text("눌러서 말하기")
-                                .underline()
-                                .foregroundColor(.white)
-                                .font(.system(size: 32, weight: .heavy))
-                            Text(isWrong ? "제시어와 달라요 다시 읽어 주세요" : "터치 후 위의 문장을 정확하게 읽어요")
-                                .foregroundColor(.white)
-                                .font(.system(size: 14, weight: .semibold))
+                // 제시어 말하기
+                ZStack {
+                    Image("Speeching")
+                        .shadow(color: Color("Orange").opacity(0.5), radius: 5)
+                    VStack {
+                        Text("내 발음")
+                            .font(.system(size: 13, weight: .semibold))
+                            .padding(.horizontal, 9)
+                            .padding(.vertical, 4)
+                            .background(Color("Orange"))
+                            .cornerRadius(15)
+                            .foregroundColor(.white)
+                        if speechRecognizer.transcript == "" {
+                            Text("버튼을 눌러 말해주세요")
+                                .opacity(0.25)
+                                .font(.system(size: 48, weight: .heavy))
+                                .frame(width: 240, height: 64)
+                                .minimumScaleFactor(0.1)
+                                .lineLimit(1)
+                        } else {
+                            Text(speechRecognizer.transcript)
+                                .font(.system(size: 48, weight: .heavy))
+                                .frame(width: 240, height: 64)
+                                .minimumScaleFactor(0.1)
+                                .multilineTextAlignment(.center)
+                                .lineLimit(2)
                         }
-                        .padding(.top)
-                    }
-                    .onTapGesture {
-                        speechTime = 100
-                        isSpeech = true
-                    }
-                } else {
-                    ZStack {
-                        Image("Speeching")
-                            .shadow(color: Color("Orange").opacity(0.5), radius: 5)
-                        VStack(spacing: 16) {
-                            Text("내 발음")
-                                .font(.system(size: 13, weight: .semibold))
-                                .padding(.horizontal, 9)
-                                .padding(.vertical, 4)
-                                .background(Color("Orange"))
-                                .cornerRadius(15)
-                                .foregroundColor(.white)
-                            if speechRecognizer.transcript == "" {
-                                Text("제시어를 읽어주세요")
-                                    .opacity(0.25)
-                                    .font(.system(size: 48, weight: .heavy))
-                                    .frame(width: 240, height: 64)
-                                    .minimumScaleFactor(0.1)
-                                    .lineLimit(1)
-                            } else {
-                                Text(speechRecognizer.transcript)
-                                    .font(.system(size: 48, weight: .heavy))
-                                    .frame(width: 240, height: 64)
-                                    .minimumScaleFactor(0.1)
-                                    .multilineTextAlignment(.center)
-                                    .lineLimit(2)
-                            }
-                            if isWrong {
-                                Text("❌ 제시어와 달라요 다시 읽어 주세요 ❌")
-                                    .font(.system(size: 13, weight: .semibold))
-                                    .padding(.vertical, 2)
-                                    .background(Color("LightRed"))
-                                    .foregroundColor(Color("Red"))
-                            }
+                        if isWrong {
+                            Text("❌ 제시어와 달라요 다시 읽어 주세요 ❌")
+                            .font(.system(size: 13, weight: .semibold))
+                            .padding(.vertical, 2)
+                            .background(Color("LightRed"))
+                            .foregroundColor(Color("Red"))
                         }
-                        .padding(.top)
                     }
-                    .padding(.top, 3)
+                    .padding(.top)
+                }
+                .padding(.top, 3)
+            }
+            if isSpeech {
+                Image("progress")
+                    .shadow(color: Color(.black).opacity(0.25),radius: 4)
+                    .overlay(
+                        ProgressView(value: speechTime, total: 100)
+                            .tint(Color("Orange_Progress"))
+                            .padding(.horizontal, 40)
+                            .padding(.top, 8)
+                            .scaleEffect(y: 2)
+                            .onReceive(progressTimer) { _ in
+                                withAnimation(.linear(duration: 1)) {
+                                    if speechTime > 0 {
+                                        speechTime -= 100/timer
+                                    }
+                                }
+                            }
+                    )
+                    .frame(height: 50)
                     .onAppear {
                         speechRecognizer.stopTranscript() //혹시라도 켜있으면 껏다다시키게
                         speechRecognizer.startTranscribing()
@@ -165,14 +167,14 @@ struct SpeechView: View {
                                 speechRecognizer.startTranscribing()
                                 isWrong = true
                                 isSpeech = false
-                                speechRecognizer.transcript = ""
+                                print(speechRecognizer.transcript)
                             }
                         }
                         let checktimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false){
                             timer in
                             let cleanedTranscript = speechRecognizer.transcript.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "")
                             //영소문자 바꾸는 거 해야함.
-
+                            
                             if(answer.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "") == cleanedTranscript){
                                 timer.invalidate()
                                 print("끝이 났어요")
@@ -183,24 +185,22 @@ struct SpeechView: View {
                     .onDisappear{
                         speechRecognizer.stopTranscript()
                     }
+            } else {
+                Button {
+                    isSpeech = true
+                    speechRecognizer.transcript = ""
+                    speechTime = 100
+                    isWrong = false
+                } label: {
+                    Text("눌러서 말하기")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: 350, alignment: .center)
+                        .frame(height: 50)
+                        .background(Color("Bg_bottom2"))
+                        .cornerRadius(12)
                 }
             }
-            Image("progress")
-                .shadow(color: Color(.black).opacity(0.25),radius: 4)
-                .overlay(
-                    ProgressView(value: speechTime, total: 100)
-                        .tint(Color("Orange_Progress"))
-                        .padding(.horizontal, 40)
-                        .padding(.top, 8)
-                        .scaleEffect(y: 2)
-                        .onReceive(progressTimer) { _ in
-                            withAnimation(.linear(duration: 1)) {
-                                if speechTime > 0 {
-                                    speechTime -= 100/timer
-                                }
-                            }
-                        }
-                )
         }
     }
 }
