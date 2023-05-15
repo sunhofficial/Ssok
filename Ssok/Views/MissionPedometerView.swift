d38482d3487a70efa2ccd31876d8aeac7ad35bb9
Pin <ppyj48625@naver.com>
2023-05-15 20:47:02 +0900

    fix : fix shaking count and sensitivity

--- Ssok/Views/MissionPedometerView.swift
+++ Ssok/Views/MissionPedometerView.swift
@@ -26,7 +26,6 @@ struct MissionPedometerView: View {
     @State var gravityz: Double = 0
     @State var backstate: Bool = false
 
-    @State var limit: Float = 100.0
     @State var progressColor: Color = Color("Progress_first")
     let more: String = "더"
     @State var ismore: Int = 0
@@ -40,7 +39,7 @@ struct MissionPedometerView: View {
             }
             VStack(spacing: 64) {
 
-                MissionTitleView(missionTitle: "춤추기 💃🕺🏻", backgroundColor: Color("MissionShake").opacity(0.28), borderColor: Color("MissionShake").opacity(0.71))
+                MissionTitleView(missionTitle: Title, backgroundColor: TitleColor.opacity(0.28), borderColor: TitleColor.opacity(0.71))
                 
                 ZStack {
                     ZStack {
@@ -49,7 +48,7 @@ struct MissionPedometerView: View {
                             .foregroundColor(Color("Gray"))
                             .shadow(color: Color.black.opacity(0.25), radius: 2, x: 0, y: -2)
                         Circle()
-                            .trim(from: 0.0, to: CGFloat(stepcount/100.0))
+                            .trim(from: 0.0, to: GoalCount == "40.0" ? CGFloat(stepcount/40.0) : CGFloat(stepcount/10.0))
                             .stroke(style: StrokeStyle(lineWidth: 25.0, lineCap: .round, lineJoin: .round))
                             .rotationEffect(.degrees(270))
                             .foregroundColor(progressColor)
@@ -57,7 +56,11 @@ struct MissionPedometerView: View {
                     
                     VStack {
                         Text("\(stepcount, specifier: "%.0f")").font(.system(size: 60, weight: .bold)) + Text("회").font(.system(size: 40, weight: .bold))
-                        Text("목표 진동수\n100회")
+                        Text("목표 진동수\n")
+                            .font(.system(size: 18))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color("GoalRed"))
+                        Text(GoalCount == "40.0" ? "40회" : "10회")
                             .font(.system(size: 18))
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color("GoalRed"))
@@ -70,12 +73,23 @@ struct MissionPedometerView: View {
                     Text(more).foregroundColor(ismore>=3 ? Color.black : Color("Gray"))
                 }.font(.system(size: 48, weight: .bold))
                 .onChange(of: stepcount) { value in
-                    if stepcount == 25{
-                        ismore += 1
-                    } else if stepcount == 50 {
-                        ismore += 1
-                    } else if stepcount == 75 {
-                        ismore += 1
+                    if GoalCount == "40.0"{
+                        if stepcount == 10{
+                            ismore += 1
+                        } else if stepcount == 20 {
+                            ismore += 1
+                        } else if stepcount == 30 {
+                            ismore += 1
+                        }
+                    }
+                    else {
+                        if stepcount == 3{
+                            ismore += 1
+                        } else if stepcount == 5 {
+                            ismore += 1
+                        } else if stepcount == 8 {
+                            ismore += 1
+                        }
                     }
                 }
             }
@@ -87,35 +101,51 @@ struct MissionPedometerView: View {
                     motionmanager.startDeviceMotionUpdates(to: OperationQueue.main) { data,error in
                         gravityx = data?.gravity.x ?? 0
                         
-                        if gravityx > 0.2 || gravityy > 0.2 || gravityz > 0.2 {
+                        if gravityx > 0.15 || gravityy > 0.15 || gravityz > 0.15 {
                             currentgravity = 1
-                        } else if gravityx <= 0.2 && gravityx >= -0.2 && gravityy <= 0.2 && gravityy >= -0.2 && gravityz <= 0.2 && gravityz >= -0.2 {
+                        } else if gravityx <= 0.15 && gravityx >= -0.15 && gravityy <= 0.15 && gravityy >= -0.15 && gravityz <= 0.15 && gravityz >= -0.15 {
                             currentgravity = 0
-                        } else if gravityx < -0.2 || gravityy < -0.2 || gravityz < 0.2 {
+                        } else if gravityx < -0.15 || gravityy < -0.15 || gravityz < 0.15 {
                             currentgravity = 2
                         }
                         
                         if currentgravity == previousgravity && previousgravity != 0 {
                             previousgravity = currentgravity
                         } else if currentgravity != previousgravity{
-                            if stepcount != 100.0 {
-                                stepcount += 1.0
+                            if GoalCount == "40.0"{
+                                if stepcount != 40.0 {
+                                    stepcount += 1.0
+                                }
+                            } else {
+                                if stepcount != 10.0{
+                                    stepcount += 1.0
+                                }
                             }
                             previousgravity = currentgravity
                         }
                     }
                     
                 }
-                
-                if stepcount <= 50 && stepcount > 25 {
-                    progressColor = Color("Progress_second")
-                } else if stepcount <= 75 && stepcount > 50 {
-                    progressColor = Color("Progress_third")
-                } else if stepcount <= 100 && stepcount > 75 {
-                    progressColor = Color("Progress_final")
+                if GoalCount == "10.0" {
+                    if stepcount <= 3 && stepcount > 1 {
+                        progressColor = Color("Progress_second")
+                    } else if stepcount <= 6 && stepcount > 3 {
+                        progressColor = Color("Progress_third")
+                    } else if stepcount <= 8 && stepcount > 6 {
+                        progressColor = Color("Progress_final")
+                    }
+                }
+                else if GoalCount == "40.0" {
+                    if stepcount <= 20 && stepcount > 10 {
+                        progressColor = Color("Progress_second")
+                    } else if stepcount <= 30 && stepcount > 20 {
+                        progressColor = Color("Progress_third")
+                    } else if stepcount <= 40 && stepcount > 30 {
+                        progressColor = Color("Progress_final")
+                    }
                 }
             }
-            if String(stepcount) == GoalCount {
+            if String(stepcount) == GoalCount  {
                 MissionCompleteView(Title: Title, background: TitleColor)
             }
         }.navigationBarHidden(true)
