52fd6b24981f73faf0369f9bc61dd26204851196
Mini <minnnidev@gmail.com>
2023-06-24 19:20:28 +0900

    refactor: remove missionColor from mission model

---
+++ Ssok/Views/Recycle/MissionDescriptionView.swift
@@ -0,0 +1,20 @@
+//
+//  MissionDescriptionView.swift
+//  Ssok
+//
+//  Created by 김민 on 2023/06/24.
+//
+
+import SwiftUI
+
+struct MissionDescriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionDescriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionDescriptionView()
+    }
+}
