f19781779b9f420e05d44e7ca74dd5beac715204
Pin <ppyj48625@naver.com>
2023-06-28 21:47:33 +0900

    feature : Add font size for variable device

---
+++ Ssok/Global/Extension/UIScreen+.swift
@@ -0,0 +1,21 @@
+//
+//  UIScreen+.swift
+//  Ssok
+//
+//  Created by 김용주 on 2023/06/28.
+//
+
+import SwiftUI
+
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
+
+    static func getWidth(_ width: CGFloat) -> CGFloat {
+        screenWidth / 390 * width
+    }
+    static func getHeight(_ height: CGFloat) -> CGFloat {
+        screenHeight / 844 * height
+    }
+}
