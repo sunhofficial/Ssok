a94f4483f3931ac9084794a2804a01541d0b2555
Pin <ppyj48625@naver.com>
2023-07-02 18:05:18 +0900

    refactor : Modify functions in MissionPedometerViewModel

--- Ssok/ViewModel/MissionPedometerViewModel.swift
+++ Ssok/ViewModel/MissionPedometerViewModel.swift
@@ -24,37 +24,50 @@ class MissionPedometerViewModel: ObservableObject {
     @Published var isMore: Int = 0
     @Published var progressColor: Color = Color("Progress_first")
     
-    func startUpdate(_ goalCount:String) {
+    func startUpdate(_ goalCount: String) {
         if motionManager.isDeviceMotionAvailable {
             motionManager.deviceMotionUpdateInterval = 0.2
             motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { data, _ in
                 self.gravityX = data?.gravity.x ?? 0
-                if self.gravityX > 0.15 || self.gravityY > 0.15 || self.gravityZ > 0.15 {
-                    self.currentGravity = 1
-                } else if abs(self.gravityX) <= 0.15 && abs(self.gravityY) <= 0.15 && abs(self.gravityZ) <= 0.15 {
-                    self.currentGravity = 0
-                } else if self.gravityX < -0.15 || self.gravityY < -0.15 || self.gravityZ < 0.15 {
-                    self.currentGravity = 2
+
+                self.checkRotation()
+                
+                self.countUp(goalCount)
+            }
+        }
+    }
+    
+    func checkRotation() {
+        if self.gravityX > 0.15 || self.gravityY > 0.15 || self.gravityZ > 0.15 {
+            self.currentGravity = 1
+        } else if abs(self.gravityX) <= 0.15 &&
+                    abs(self.gravityY) <= 0.15 &&
+                    abs(self.gravityZ) <= 0.15 {
+            self.currentGravity = 0
+        } else if self.gravityX < -0.15 || self.gravityY < -0.15 || self.gravityZ < 0.15 {
+            self.currentGravity = 2
+        }
+    }
+    
+    func countUp(_ goalCount:String) {
+        if self.currentGravity == self.previousGravity &&
+            self.self.previousGravity != 0 {
+            self.previousGravity = self.currentGravity
+        } else if self.currentGravity != self.previousGravity {
+            if goalCount == "40.0"{
+                if self.stepCount != 40.0 {
+                    self.stepCount += 1.0
+                } else if self.stepCount == 40.0 {
+                    self.motionManager.stopDeviceMotionUpdates()
                 }
-                if self.currentGravity == self.previousGravity && self.self.previousGravity != 0 {
-                    self.previousGravity = self.currentGravity
-                } else if self.currentGravity != self.previousGravity {
-                    if goalCount == "40.0"{
-                        if self.stepCount != 40.0 {
-                            self.stepCount += 1.0
-                        } else if self.stepCount == 40.0 {
-                            self.motionManager.stopDeviceMotionUpdates()
-                        }
-                    } else {
-                        if self.stepCount != 10.0 {
-                            self.stepCount += 1.0
-                        } else if self.stepCount == 10.0 {
-                            self.motionManager.stopDeviceMotionUpdates()
-                        }
-                    }
-                    self.previousGravity = self.currentGravity
+            } else {
+                if self.stepCount != 10.0 {
+                    self.stepCount += 1.0
+                } else if self.stepCount == 10.0 {
+                    self.motionManager.stopDeviceMotionUpdates()
                 }
             }
+            self.previousGravity = self.currentGravity
         }
     }
 }
